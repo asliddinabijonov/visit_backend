0c6622f947e11b00be7e6dd8d5f98009e902acee
asliddinabijonov <asliddinabijonov04@gmail.com>
2026-03-27 17:10:16 +0500

    oxirgisi

--- api/index.py
+++ api/index.py
@@ -1,8 +1,18 @@
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
-
+project_root = Path(__file__).resolve().parent
+sys.path.insert(0, str(project_root))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+import django
+django.setup()
+
+from django.core.wsgi import get_wsgi_application
+
 app = get_wsgi_application()
+
+def handler(request, context=None):
+    return app(request, context)
